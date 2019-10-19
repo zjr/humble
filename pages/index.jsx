b99e2de1dcd9a478b8117550bd2ac1e4e14d5b6d
Zachary J. Rollyson <me@zjr.io>
2019-10-19 13:22:19 -0400

    feat: connect to ghost api

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import GhostContentAPI from '@tryghost/content-api';
 
-function Home() {
-	return <div>Welcome to Next.js!</div>;
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+	url: 'https://humbleghost.herokuapp.com',
+	key: '543271aba1b92a2286308226c7',
+	version: 'v2'
+});
+
+Home.getInitialProps = async () => {
+	const posts = await api.posts
+		.browse({
+			limit: 'all'
+		})
+		.catch(err => {
+			console.error(err);
+		});
+
+	return { posts };
+};
+
+export default function Home({ posts }) {
+	return (
+		<div>
+			<h1>Welcome to Next.js!</h1>
+			<ul>
+				{posts.map(post => (
+					<li key={post.slug}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
-
-export default Home;
