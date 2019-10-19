6c96dbcfcb444d760b51edaf43c83e8cb94979fa
Zachary J. Rollyson <me@zjr.io>
2019-10-19 18:41:59 -0400

    Revert "Merge pull request #13 from zjr/feature/about"
    This reverts commit 4cc74ef35761050ee73a7296fa47a5d205692238, reversing
    changes made to cbaf1b220935bd3b57a13b91c2d3d23eae457d26.

--- components/WhiteBlock/WhiteBlock.jsx
+++ components/WhiteBlock/WhiteBlock.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import Link from 'next/link';
-
-import HButton from '../HButton/Hbutton';
 
 import './WhiteBlock.scss';
+import HButton from '../HButton/Hbutton';
 
-export default function WhiteBlock({ head, main, body, button, link, as }) {
+export default function WhiteBlock({ head, main, body, button, link }) {
 	return (
 		<div className="white-block">
 			<h5>{head}</h5>
 			<h2>{main}</h2>
 			<p>{body}</p>
 			{button && (
-				<Link href={link} as={as}>
-					<a>
-						<HButton dark text={button} />
-					</a>
-				</Link>
+				<a href={link}>
+					<HButton dark text={button} />
+				</a>
 			)}
 		</div>
 	);
--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
 
 import ghost from '../config/ghost';
 import Layout from '../components/layout';
-
-import '../styles/page.scss';
 
 Page.getInitialProps = async req => {
 	return {
@@ -13,38 +12,14 @@ Page.getInitialProps = async req => {
 
 export default function Page({ page }) {
 	// See here for other page information
-	console.log(page);
+	// console.log(page);
 
 	return (
-		<Layout title={page.title} className="page">
-			<div className="hero">
-				<div className="menu">
-					<div className="hamburger">
-						<div />
-						<div />
-						<div />
-					</div>
-					<div className="right">
-						<img src="/images/HumbleHairdressersLogoLight.png" alt="h arrow" />
-						<h5>humble hairdressers</h5>
-					</div>
-				</div>
-				<div className="text-content">
-					<h1>{page.title}</h1>
-					<p className="excerpt">{page.custom_excerpt || page.excerpt}</p>
-				</div>
-			</div>
-			<div className="sub-hero">
-				<div className="feature-block">
-					<h3 className="title">The people are amazing.</h3>
-					<p className="text">
-						Don’t get us wrong, it’s hard work. But the gratification is worth
-						it. Nothing is cooler than showing up and delivering one when no one
-						is expecting it.
-					</p>
-				</div>
-				<img className="feature-image" src={page.feature_image} alt="" />
-			</div>
+		<Layout title={page.title}>
+			<h1>{page.title}</h1>
+			<Link href="/">
+				<a>Home</a>
+			</Link>
 			<div dangerouslySetInnerHTML={{ __html: page.html }} />
 		</Layout>
 	);
