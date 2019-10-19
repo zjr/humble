b91afb7cf8d16e3aee0228b6e8ad894e44a58b73
Zachary J. Rollyson <me@zjr.io>
2019-10-19 18:44:09 -0400

    refactor: small cleanup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Link from 'next/link';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 
-import ghost from '../config/ghost';
 import Layout from '../components/layout';
 import HButton from '../components/HButton/Hbutton';
 import WhiteBlock from '../components/WhiteBlock/WhiteBlock';
 import DonationsBlock from '../components/DonationsBlock/DonationsBlock';
 import Footer from '../components/Footer/Footer';
 
-Home.getInitialProps = async () => {
-	const data = await ghost.posts
-		.browse({ fields: 'id, title' })
-		.catch(console.error);
-
-	return { posts: data, meta: data.meta };
-};
-
-export default function Home({ posts, meta }) {
-	// More info on posts & paging:
-	// console.log({ posts, meta });
-
-	// noinspection HtmlUnknownTarget
+export default function Home() {
 	const [navOpen, setNavOpen] = useState(false);
 
 	const navClasses = classNames('nav', {
 		open: navOpen
 	});
 
+	// noinspection HtmlUnknownTarget
 	return (
 		<Layout>
 			<nav className={navClasses} onClick={() => setNavOpen(false)}>
@@ -58,9 +45,6 @@ export default function Home({ posts, meta }) {
 						</div>
 					</div>
 					<h1>We provide quality, no-charge services for those in need.</h1>
-					<Link href="/[slug]" as="/about">
-						<a>About</a>
-					</Link>
 					<div className="home-ctas">
 						<HButton text="Donate" />
 						<HButton text="Volunteer" />
@@ -95,12 +79,6 @@ export default function Home({ posts, meta }) {
 					/>
 					<DonationsBlock />
 				</div>
-
-				{/*<ul>*/}
-				{/*	{posts.map(post => (*/}
-				{/*		<li key={post.slug}>{post.title}</li>*/}
-				{/*	))}*/}
-				{/*</ul>*/}
 			</div>
 			<Footer />
 		</Layout>
