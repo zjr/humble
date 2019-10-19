af66ba36c664240cf40f6a36a3018a41aee4d382
Zachary J. Rollyson <me@zjr.io>
2019-10-19 09:43:10 -0400

    chore: add release mgmt

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  tabs: true,
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'none'
+	tabs: true,
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none'
 };
