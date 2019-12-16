afce2c0586b8eb3986c6c398ca113e304777467b
Zachary J. Rollyson <me@zjr.io>
2019-12-16 12:52:47 -0500

    chore: add versionrc?

---
+++ .versionrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	types: [
+		{ type: 'feat', section: 'Features' },
+		{ type: 'fix', section: 'Bug Fixes' },
+
+		{ type: 'build', section: 'Other' },
+		{ type: 'perf', section: 'Other' },
+		{ type: 'test', section: 'Other' },
+
+		{ type: 'chore', section: 'Other' },
+		{ type: 'docs', section: 'Other' },
+
+		{ type: 'refactor', section: 'Other' },
+		{ type: 'style', section: 'Other' },
+
+		{ type: 'ci', hidden: true },
+		{ type: 'revert', hidden: true }
+	]
+};
